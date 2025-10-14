3aa743c8e24a1260734c98e5c7ff1ef0d6cddd9c
Trymad <wolfesss2015@gmail.com>
2025-10-15 02:07:02 +0300

    boardstate in boardbiew

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,16 +1,29 @@
 <script setup lang="ts">
 
+import { useBoard, type BoardState } from '@/composables/useBoard'
 import { BoardApi, TheChessboard, type BoardConfig, type MoveEvent } from 'vue3-chessboard'
 import 'vue3-chessboard/style.css'
 
 interface Props {
-  boardCreated?: (api: BoardApi) => void
+  boardCreated?: (api: BoardState) => void
   playerColor?: "white" | "black" | undefined
   move?: (move: MoveEvent) => void
   boardConfig?: BoardConfig
 }
 
 const props = defineProps<Props>();
+
+const boardState = useBoard()
+
+const onCreated = (api: BoardApi) => {
+  boardState.setBoardApi(api)
+  props.boardCreated?.(boardState)
+}
+
+const onMoved = (move: MoveEvent) => {
+  boardState.updateState()
+  props.move?.(move)
+}
 
 </script>
 
@@ -19,8 +32,8 @@ const props = defineProps<Props>();
   reactive-config
   :player-color="props.playerColor"
   :board-config="props.boardConfig"
-  @board-created="props.boardCreated"
-  @move="props.move"/>
+  @board-created="onCreated"
+  @move="onMoved"/>
 </template>
 
 <style scoped>
