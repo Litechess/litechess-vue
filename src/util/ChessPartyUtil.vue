dc6feabe79a282b956fabcffee01f73bff730d51
Trymad <wolfesss2015@gmail.com>
2025-11-25 23:24:51 +0300

    match history view

---
+++ src/util/ChessPartyUtil.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import type { GameStatus } from '@/types/ChessParty';
+
+const GAME_STATUSES: Record<GameStatus, GameStatus> = {
+  WIN_WHITE: 'WIN_WHITE',
+  WIN_BLACK: 'WIN_BLACK',
+  DRAW: 'DRAW',
+  NOT_FINISHED: 'NOT_FINISHED',
+  TIMEOUT_WIN_BLACK: 'WIN_BLACK',
+  TIMEOUT_WIN_WHITE: 'WIN_WHITE',
+  SURRENDER_WIN_BLACK: 'WIN_BLACK',
+  SURRENDER_WIN_WHITE: 'WIN_WHITE'
+}
+
+export function getSimpleStatus(status: GameStatus): GameStatus {
+  if(status == 'NOT_FINISHED') return 'NOT_FINISHED'
+  return GAME_STATUSES[status];
+}
+
+</script>
