669e05d257336e575dd756003bef08d0e042bd72
Trymad <wolfesss2015@gmail.com>
2025-12-13 00:26:14 +0300

    remove hardcode url

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,35 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from "vite"
+import vue from "@vitejs/plugin-vue"
+import vueDevTools from "vite-plugin-vue-devtools"
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+    server: mode === "development"
+      ? {
+          proxy: {
+            "/api": {
+              target: env.VITE_API_TARGET,
+              changeOrigin: true,
+            },
+            "/identity": {
+              target: env.VITE_IDENTITY_TARGET,
+              changeOrigin: true,
+              rewrite: (path) => path.replace(/^\/identity/, ''),
+            },
+          },
+        }
+      : undefined,
+  }
 })
