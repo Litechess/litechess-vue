04375c3eccaf72aa292cff26e5c4a716453a60a6
Trymad <wolfesss2015@gmail.com>
2025-12-15 02:00:30 +0300

    remove vite proxy, migrate to nginx proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import vueDevTools from "vite-plugin-vue-devtools"
 import { fileURLToPath, URL } from "node:url"
-import fs from "fs"
-import path from 'path'
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
+export default defineConfig(() => {
 
   return {
     plugins: [
@@ -18,31 +14,5 @@ export default defineConfig(({ mode }) => {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
-    server: mode === "development"
-      ? {
-          proxy: {
-            "/api": {
-              target: env.VITE_API_TARGET,
-              changeOrigin: true,
-            },
-            "/ws": {
-              target: env.VITE_WS_TARGET,
-              changeOrigin: true,
-              ws: true
-            },
-            "/identity": {
-              target: env.VITE_IDENTITY_TARGET,
-              secure: false,
-              changeOrigin: true,
-
-              rewrite: (path) => path.replace(/^\/identity/, ''),
-            },
-          },
-          https: {
-            key: fs.readFileSync(path.resolve(__dirname, env.VITE_SSL_KEY_PATH)),
-            cert: fs.readFileSync(path.resolve(__dirname, env.VITE_SSL_CERT_PATH)),
-          },
-        }
-      : undefined,
   }
 })
